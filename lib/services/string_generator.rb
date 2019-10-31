10c2f509b683109bd4b9ce6ee68de825ce75a86b
Mikalai Yankouski <mikolay.yankovsky@mail.ru>
2019-11-01 00:30:11 +0300

    added prototype

--- lib/services/string_generator.rb
+++ lib/services/string_generator.rb
@@ -5,9 +5,9 @@ class StringGenerator
 
   BOTTLES = 'bottles'
   BOTTLE = 'bottle'
-  DEFAULT_PRONOUN = 'Take it'
-  ANOTHER_PRONOUN = 'Go to the store'
-  PRONOUN = 'Take one'
+  DEFAULT_PRONOUN = 'Take it down'
+  ANOTHER_PRONOUN = 'Go to the store down'
+  PRONOUN = 'Take one down'
   ZERO_TITLE = 'No more'
 
   BOTTLES_ARR = (0...100).to_a
@@ -30,7 +30,7 @@ class StringGenerator
         bottle: BOTTLES,
         pronoun: ANOTHER_PRONOUN,
         text: ZERO_TITLE.downcase,
-        result: 'no more bottles'
+        result: ending_row(BOTTLE)
       }
     elsif number == 1
       {
@@ -38,7 +38,15 @@ class StringGenerator
         bottle: BOTTLE,
         pronoun: DEFAULT_PRONOUN,
         text: number,
-        result: ending_row
+        result: 'no more bottles'
+      }
+    elsif number == 2
+      {
+        title: number,
+        bottle: BOTTLES,
+        pronoun: PRONOUN,
+        text: number,
+        result: ending_row(BOTTLE)
       }
     else
       {
@@ -46,25 +54,19 @@ class StringGenerator
         bottle: BOTTLES,
         pronoun: PRONOUN,
         text: number,
-        result: ending_row
+        result: ending_row(BOTTLES)
       }
     end
   end
 
-  def ending_row
-    if number == 1
-      'no more bottles'
-    elsif number == 2
-      "#{BOTTLES_ARR[(number - 1)]} bottle"
-    else
-      "#{BOTTLES_ARR[(number - 1)]} bottles"
-    end
-
+  def ending_row(form)
+    p form
+    "#{BOTTLES_ARR[(number - 1)]} #{form}"
   end
 
   def string
     p number.zero?
     "#{values[:title]} #{values[:bottle] } of beer on the wall, #{values[:text]} #{values[:bottle]  } of beer.\n" +
-    "#{values[:pronoun]} down and pass it around, #{values[:result]} of beer on the wall.\n"
+    "#{values[:pronoun]} and pass it around, #{values[:result]} of beer on the wall.\n"
   end
 end
