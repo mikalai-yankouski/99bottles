70d7356f74a219550ca2bed60bb306c0657492e3
Mikalai Yankouski <mikolay.yankovsky@mail.ru>
2019-11-01 00:19:38 +0300

    change some params

--- lib/services/string_generator.rb
+++ lib/services/string_generator.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 class StringGenerator
-  attr_reader :number
+  attr_reader :number, :values
+
+  BOTTLES = 'bottles'
+  BOTTLE = 'bottle'
+  DEFAULT_PRONOUN = 'Take it'
+  ANOTHER_PRONOUN = 'Go to the store'
+  PRONOUN = 'Take one'
+  ZERO_TITLE = 'No more'
+
+  BOTTLES_ARR = (0...100).to_a
 
   def initialize(number)
     @number = number
+    @values = genreate_default_values
   end
 
   def call
@@ -13,8 +23,48 @@ class StringGenerator
 
   private
 
+  def genreate_default_values
+    if number == 0
+      {
+        title: ZERO_TITLE,
+        bottle: BOTTLES,
+        pronoun: ANOTHER_PRONOUN,
+        text: ZERO_TITLE.downcase,
+        result: 'no more bottles'
+      }
+    elsif number == 1
+      {
+        title: number,
+        bottle: BOTTLE,
+        pronoun: DEFAULT_PRONOUN,
+        text: number,
+        result: ending_row
+      }
+    else
+      {
+        title: number,
+        bottle: BOTTLES,
+        pronoun: PRONOUN,
+        text: number,
+        result: ending_row
+      }
+    end
+  end
+
+  def ending_row
+    if number == 1
+      'no more bottles'
+    elsif number == 2
+      "#{BOTTLES_ARR[(number - 1)]} bottle"
+    else
+      "#{BOTTLES_ARR[(number - 1)]} bottles"
+    end
+
+  end
+
   def string
-    "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-    "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+    p number.zero?
+    "#{values[:title]} #{values[:bottle] } of beer on the wall, #{values[:text]} #{values[:bottle]  } of beer.\n" +
+    "#{values[:pronoun]} down and pass it around, #{values[:result]} of beer on the wall.\n"
   end
 end
