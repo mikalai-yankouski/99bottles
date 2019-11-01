19b66cb26b4c9a17f5cc407e3805319925fe1d43
mikalai-yankouski <mikolay.yankovsky@mail.ru>
2019-11-01 13:30:34 +0300

    My solution

--- lib/services/song_iterator.rb
+++ lib/services/song_iterator.rb
@@ -5,13 +5,15 @@ require './lib/services/string_generator.rb'
 class SongIterator
   attr_reader :params
 
-  def initialize(params)
-    @params = params
+  def initialize(*params)
+    @params = params.flatten
   end
 
   def call
+    song = []
     params.each do |number|
-      return StringGenerator.new(number).call
+      song.push(StringGenerator.new(number).call)
     end
+    song.join("\n")
   end
 end
--- lib/services/string_generator.rb
+++ lib/services/string_generator.rb
@@ -1,72 +1,46 @@
 # frozen_string_literal: true
 
 class StringGenerator
-  attr_reader :number, :values
+  attr_reader :number, :string_params, :remainder
 
   BOTTLES = 'bottles'
   BOTTLE = 'bottle'
-  DEFAULT_PRONOUN = 'Take it down'
-  ANOTHER_PRONOUN = 'Go to the store down'
-  PRONOUN = 'Take one down'
-  ZERO_TITLE = 'No more'
-
-  BOTTLES_ARR = (0...100).to_a
 
   def initialize(number)
     @number = number
-    @values = genreate_default_values
+    @remainder = @number - 1
   end
 
   def call
-    string
+    return number_zero_string if number.zero?
+    return remainder_zero_string if remainder.zero?
+    default_string
   end
 
   private
 
-  def genreate_default_values
-    if number == 0
-      {
-        title: ZERO_TITLE,
-        bottle: BOTTLES,
-        pronoun: ANOTHER_PRONOUN,
-        text: ZERO_TITLE.downcase,
-        result: ending_row(BOTTLE)
-      }
-    elsif number == 1
-      {
-        title: number,
-        bottle: BOTTLE,
-        pronoun: DEFAULT_PRONOUN,
-        text: number,
-        result: 'no more bottles'
-      }
-    elsif number == 2
-      {
-        title: number,
-        bottle: BOTTLES,
-        pronoun: PRONOUN,
-        text: number,
-        result: ending_row(BOTTLE)
-      }
-    else
-      {
-        title: number,
-        bottle: BOTTLES,
-        pronoun: PRONOUN,
-        text: number,
-        result: ending_row(BOTTLES)
-      }
-    end
+  def number_zero_string
+    "No more bottles of beer on the wall, no more bottles of beer.\n" \
+    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
 
-  def ending_row(form)
-    p form
-    "#{BOTTLES_ARR[(number - 1)]} #{form}"
+  def remainder_zero_string
+    "#{number} #{bottles_one?(number)} of beer on the wall, #{number} #{bottles_one?(number)} of beer.\n" \
+    "Take it down and pass it around, no more bottles of beer on the wall.\n"
   end
 
-  def string
-    p number.zero?
-    "#{values[:title]} #{values[:bottle] } of beer on the wall, #{values[:text]} #{values[:bottle]  } of beer.\n" +
-    "#{values[:pronoun]} and pass it around, #{values[:result]} of beer on the wall.\n"
+  def default_string
+    "#{number} #{bottles_one?(number)} of beer on the wall, #{number} #{bottles_one?(number)} of beer.\n" \
+    "Take one down and pass it around, #{remainder} #{bottles_one?(remainder)} of beer on the wall.\n"
   end
+
+  def bottles_one?(number)
+    number == 1 ? BOTTLE : BOTTLES
+  end
+
+  # def string
+  #   "#{string_params[:title]} #{string_params[:bottle]} of beer on the wall, " \
+  #   "#{string_params[:text]} #{string_params[:bottle]} of beer.\n" \
+  #   "#{string_params[:pronoun]} #{string_params[:middleware]}, #{string_params[:ending]} of beer on the wall.\n"
+  # end
 end
